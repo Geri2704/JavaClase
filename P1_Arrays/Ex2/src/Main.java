505d8dfeaa5cabedc4ae0471c6efbae753bf0375
Geri2704 <73889365+Geri2704@users.noreply.github.com>
2022-10-03 08:37:23 +0200

    Ejercicio 2 Completado

---
+++ P1_Arrays/Ex2/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        buscarLetra();
+    }
+
+    private static void buscarLetra() {
+        char letras[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba su numero de DNI: ");
+        int dni = sc.nextInt();
+        int resto = dni%23;
+        System.out.println("Su letra es: " + letras[resto]);
+    }
+}
